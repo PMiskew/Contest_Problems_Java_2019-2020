b65e137584e20e30f5f837b44f2b4577d760bb4e
PMiskew <paul.miskew@gmail.com>
2019-10-02 15:01:20 -0400

    add contest template, add CCC2012

---
+++ src/CCCJ4BigBang2012.java
@@ -0,0 +1,52 @@
+
+public class CCCJ4BigBang2012 {
+/*
+ * 
+ * Link:
+ * https://cemc.uwaterloo.ca/contests/computing/2012/stage1/juniorEn.pdf
+ * 
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(bigBang());
+	}
+	
+	public static String bigBang() {
+		
+		String[] letters = {"A","B","C","D","E","F","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+		int k = 3;
+		String input = "FXAB";
+		
+		String result = "";
+		
+		for (int i = 0; i < input.length(); i = i + 1) {
+		
+			String clet = "" + input.charAt(i);
+			int loc = 0;
+			
+			for (int j = 0; j < letters.length; j = j + 1) {
+				
+				if (letters[j].equals(clet)) {
+					loc = j;
+					break;
+				}
+				
+			}
+			
+			int s = 3*(i + 1) + k;
+			System.out.println(loc);
+			System.out.println(s);
+			
+			if (s > loc) {
+				s 
+			}
+			result = result + letters[(loc - s)];
+			
+		
+		}
+		
+		return result;
+		
+	}
+
+}
---
+++ src/ContestTemplate.java
@@ -0,0 +1,18 @@
+
+public class ContestTemplate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Advice 1: GET OUT THE MAIN!
+		System.out.println(question());
+	}
+	
+	public static String question() {
+		//Advice 2: Don't take inputs until you are done. 
+		String input = "SHINS";
+		
+		
+		return "NO";
+	}
+
+}
